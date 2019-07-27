d7a60ef63814fdcd9e72640d889e972552e0c927
Rajesh Kumar <mintooraj123@gmail.com>
2019-07-27 18:32:02 -0400

    Added node install script

--- install_k8s_node.sh
+++ install_k8s_node.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Shell Script to setup the kubernetes node.
 #This Script has to be executed on Kubernetes worker node.
-#Auther : Rajesh Kumar
+#Author : Rajesh Kumar
 
 echo "Setting up the pre-requsite for the install"
 echo "###########################################"
@@ -15,6 +15,31 @@ firewall-cmd --permanent --add-port=30000-32767/tcp
 firewall-cmd --permanent --add-port=6783/tcp
 firewall-cmd  --reload
 echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
+
+#Checking the required CPUs for install
+
+CPU_COUNT=`nproc`
+echo $CPU_COUNT  >/dev/null 2>&1
+
+if [ $CPU_COUNT -lt 2 ]
+ then
+
+    echo "Sorry Your Server is not having required minimum CPU(2), Script now will exit"
+    exit
+
+else
+    echo "Required minium CPUs are Validated.. Going Ahead with Install"
+fi
+
+##Add the HOSTS file entry with IP and the hostname
+
+IP=`hostname -I | awk '{print $1}'`
+HOST="node-01"
+echo  "$IP $HOST" > /etc/hosts
+
+
+#Turning off SWAP
+swapoff -a
 
 # Configure the Kubernetes Repository
 
